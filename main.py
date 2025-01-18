e0c6d24f03ec68a6526223dceb4519f31b047aa0
Afonso <afonso.canibulas@gmail.com>
2025-01-18 21:07:48 +0000

    Update main.py
    adicionadas as classes

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-print("Teste")
+"""
+------------------------------------------------------
+Projeto do presente: Orpheus AI 
+
+*** AI BOT ***
+
+AUTHORS IDENTIFICATION 
+  - Afonso Ferreira
+
+Comments:
+- Some function comments were made by AI
+------------------------------------------------------
+
+======================================================
+CHANGELOG:
+18/01/2025 Criação das classes games, AI_chat_bot, program_data e UI
+======================================================
+
+"""
+
+class games:
+  def __init__():
+    pass
+
+
+class AI_chat_bot:
+    def __init__():
+      pass
+
+class program_data:
+    def __init__():
+      pass
+
+class UI:
+  def __init__():
+      pass
+  
